f3700b260111ab62ce0e2dc61075848c1ea6c3f9
Babu Balakrishnan <babu.balakrishnan@digiapt.com>
2020-08-26 15:37:54 +0530

    implemented get all cities API & updated postman collection

---
+++ controllers/utils.js
@@ -0,0 +1,35 @@
+const log = require('../utils/logger').get();
+config = require('config');
+const FILE_INFO = 'Utils Controller';
+
+const db = require('../models');
+const cityMasterModel = db.city_master;
+
+
+var getAllCities = (req,res) => {
+    const FUN_LABEL = `\n\t getAllCities ${FILE_INFO} \n\t`; 
+    let response = {};
+    log.info(`${FUN_LABEL} IN`);
+    log.info(`${FUN_LABEL} req params ${JSON.stringify(req.params)}`);
+    log.info(`${FUN_LABEL} req query ${JSON.stringify(req.query)}`);
+    cityMasterModel.findAndCountAll().then(result=> {
+        log.info(`${FUN_LABEL} got result for cityMasterModel.findAndCountAll`);
+        log.debug(result);
+        response.cities = result.rows;
+        response.count = result.count;
+        log.info(`${FUN_LABEL} OUT`);
+        res.status(200).send(response);
+    }).catch(err=>{
+        log.error(`${FUN_LABEL} error in cityMasterModel.findAndCountAll`);
+        log.error(err);
+        log.info(`${FUN_LABEL} OUT`);
+        response.code = 'error_fetch_cities';
+        response.message = 'Unable to fetch cities';
+        response.error_details = err;
+        return res.status(500).send(response);
+    })
+}
+
+module.exports = {
+    getAllCities
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var utilController = require('../controllers/utils');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Prism Johnson API Server' });
 });
+
+/* GET all cities */
+router.get('/cities', utilController.getAllCities);
 
 module.exports = router;
