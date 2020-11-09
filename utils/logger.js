a356b7c473c3ea13ab356aeb2fe69e040a80030a
Muthu <muthu.r@digiapt.com>
2020-11-09 19:02:17 +0530

    added logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,11 +1,14 @@
 config = require('config');
 const FILE_INFO = 'Logger Utils';
-
+opts = {
+    logFilePath:'./mylogfile.log',
+    timestampFormat:'YYYY-MM-DD HH:mm:ss.SSS'
+}
 
 var get = () => {
     const FUN_LABEL = `\n\t getLogger ${FILE_INFO} \n\t`; 
     // console.log(`${FUN_LABEL} IN`);
-    const log = require('simple-node-logger').createSimpleLogger();
+    const log = require('simple-node-logger').createSimpleLogger(opts);
     log.setLevel(config.log_level);
     // console.log(`${FUN_LABEL} OUT`);
     return log;
