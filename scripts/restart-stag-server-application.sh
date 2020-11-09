63d3c9fc128c0d454ae5eec4265818e230fbaef9
Muthu <muthu.r@digiapt.com>
2020-11-09 12:10:08 +0530

    updated scripts for staging

--- scripts/restart-stag-server-application.sh
+++ scripts/restart-stag-server-application.sh
@@ -1,2 +1,2 @@
-PORT=8112 NODE_ENV=staging DEBUG=digiapt-prism-johnson-backend:* forever stop bin/www
-PORT=8112 NODE_ENV=staging DEBUG=digiapt-prism-johnson-backend:* forever start bin/www
+PORT=8112 NODE_ENV=staging DEBUG=api-prism:* forever stop bin/www
+PORT=8112 NODE_ENV=staging DEBUG=api-prism:* forever start bin/www
