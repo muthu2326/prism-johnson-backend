ba5bc78b3b92e8edb7bbac46bb270ede36a2b97c
Muthu <muthu.r@digiapt.com>
2020-11-19 13:23:50 +0530

    reset link updated

--- controllers/auth.server.controllers.js
+++ controllers/auth.server.controllers.js
@@ -196,7 +196,7 @@ exports.forgotPassword = function(req, res) {
                 console.log(dealer);
                 if(dealer != null){
                     let name = user.dataValues.name
-                    let body = `<html><body>Hi ${name}, <br><br>Please click here to reset your password <a href="">Click here<></body></html>`
+                    let body = `<html><body>Hi ${name}, <br><br>Please click here to reset your password <a href="http://45.56.64.148:9192/auth/reset-pass">Click here<></body></html>`
                     sendEmail(email, "Reset Password",body)
                     console.log('sent email')
                     res.status(200).jsonp({
